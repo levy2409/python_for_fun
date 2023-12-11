2dc643e6f20cfffe000f2229d6ad5a97ea9e1f61
levy2409 <52308952+levy2409@users.noreply.github.com>
2023-12-11 14:57:39 +0100

    py file

---
+++ py.py
@@ -0,0 +1,43 @@
+import random
+import csv
+data_base = "data_base.csv"
+
+def nombre():
+    nom_user = input("Entrez votre nom : ")
+    nombre_aleatoire = random.randint(1, 1000)
+    print(nombre_aleatoire)
+    tentative = 0
+    a = True
+    while(a):
+        chiffre_saisi = int(input("Entrez un chiffre : "))
+        if chiffre_saisi == nombre_aleatoire:
+            a = False
+        elif chiffre_saisi < nombre_aleatoire:
+            print('chiffre plus grand')
+            tentative = tentative + 1
+        else:
+            print('chiffre plus petit')
+            tentative = tentative + 1
+    # print(tentative)
+    print(f'Bravo {nom_user} gagnere au bout de {tentative}')
+    with open(data_base, mode='a', newline='', encoding='utf-8') as fichier:
+        writer = csv.writer(fichier)
+        writer.writerow([nom_user, tentative])
+
+def affichage_user():
+    with open(data_base, mode='r', encoding='utf-8') as fichier:
+        reader = csv.reader(fichier)
+
+        # Lire et afficher chaque ligne du fichier
+        for ligne in reader:
+            print(ligne)  # Affiche la ligne sous forme de liste
+
+menu_bol = True
+while menu_bol:
+    menu = int(input("Que voulais vous faire \n1 Jouer \n2 afficher les joueurs\n3 exit : "))
+    if menu == 1:
+        nombre()
+    elif menu == 2:
+        affichage_user()
+    else:
+        menu_bol =False
